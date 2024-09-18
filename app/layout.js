a371adfa42efd58bb19c9572337684e6c4e527c3
Nancymmm <706369937@qq.com>
2024-09-18 11:07:40 +0800

    practice 8: 授权登录

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import './style.css';
 import Sidebar from '@/components/Sidebar';
+import Header from '@/components/Header';
 
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <div className="container">
+          <Header />
           <div className="main">
             <Sidebar />
             <section className="col note-viewer">{children}</section>
